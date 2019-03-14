2bd94b787154db9063ce654f27a2bd63d39300e8
정재민 <jaemin032@naver.com>
2019-03-14 18:22:20 +0900

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,17 @@
+
+
+def sequence_preprocess(data, input_length, output_length):
+    if input_length > len(data) or output_length > len(data):
+        raise ValueError('Sequence length cannot be longer than data')
+
+    input_sequences, output_sequences = [], []
+    for i in range(len(data)):
+        if i + input_length + output_length > len(data):
+            break
+        input_sequences.append(data[i:i + input_length])
+        output_sequences.append(data[i+input_length:i+input_length+output_length])
+
+    return input_sequences, output_sequences
+
+
+
